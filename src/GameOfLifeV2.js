740f8c0db8ca08039bdde22e65faa167023fec5f
epoxi117 <gplacid2@calstatela.edu>
2020-07-16 04:28:15 -0700

    Animation is only working for mobile at the moment

--- src/GameOfLifeV2.js
+++ src/GameOfLifeV2.js
@@ -1,24 +1,60 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Menu from "./Menu.js";
 
 const Cell = (props) => {
-  const { alive, newBorn } = props;
+  const { key, newBorn, alive } = props;
   return (
     <div
-      className={`${newBorn ? "newBorn" : ""} ${alive ? "alive" : ""}`}></div>
+      className={`cell ${newBorn ? "newBorn" : ""} ${alive ? "alive" : ""}`}
+      id={key}></div>
   );
 };
 
 const GameOfLifeV2 = () => {
   const [windowWidth, setWindowWidth] = useState(0);
   const [windowHeight, setWindowHeight] = useState(0);
-  const [cellCount, setCellCount] = useState(399); //default for mobile
+  const [cellCount, setCellCount] = useState(1100); //default for mobile
   const [gridWidth, setGridWidth] = useState(40);
   const [gridHeight, setGridHeight] = useState(25);
   const [cellGrid, setGrid] = useState([]);
   const [generationCount, setGenerationCount] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
+  const [isRunning, setIsRunning] = useState(true);
   const [delay, setDelay] = useState(100);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+      setWindowHeight(window.innerHeight);
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+    handleResize();
+  });
+
+  useEffect(() => {
+    function determineCellCount() {
+      if (windowWidth < 768) {
+        setCellCount(399);
+        setGridWidth(19);
+        setGridHeight(21);
+      }
+      if (windowWidth >= 768) {
+        setCellCount(1100);
+        setGridWidth(46);
+        setGridHeight(25);
+      }
+      if (windowWidth >= 1200) {
+        setCellCount(1608);
+        setGridWidth(67);
+        setGridHeight(24);
+      }
+    }
+
+    determineCellCount();
+  }, [windowWidth]);
 
   const initializeGrid = () => {
     let grid = [];
@@ -62,67 +98,105 @@ const GameOfLifeV2 = () => {
     }
 
     randomizeGrid();
-  }, []);
+  }, [windowWidth]);
 
-  useEffect(() => {
-    function handleResize() {
-      setWindowWidth(window.innerWidth);
-      setWindowHeight(window.innerHeight);
-      window.addEventListener("resize", handleResize);
+  const calculateNeighbors = (currentGrid, ii, jj) => {
+    //Rules for Torodial World
+    let topRow = ii - 1 < 0 ? gridHeight - 1 : ii - 1;
+    let bottomRow = ii + 1 >= gridHeight ? 0 : ii + 1;
+    let leftColumn = jj - 1 < 0 ? gridWidth - 1 : jj - 1;
+    let rightColumn = jj + 1 >= gridWidth ? 0 : jj + 1;
 
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }
-    handleResize();
-  });
+    let total = 0;
 
-  useEffect(() => {
-    function determineCellCount() {
-      if (windowWidth < 768) {
-        setCellCount(399);
-        setGridWidth(19);
-        setGridHeight(21);
-      }
-      if (windowWidth >= 768) {
-        setCellCount(1100);
-        setGridWidth(44);
-        setGridHeight(25);
-      }
-      if (windowWidth >= 1200) {
-        setCellCount(1608);
-        setGridWidth(67);
-        setGridHeight(24);
-      }
-    }
-    determineCellCount();
-    createCells();
-  });
-  const createCells = () => {
-    let cellArray = [];
+    total += currentGrid[topRow][leftColumn].status ? 1 : 0;
+    total += currentGrid[topRow][jj].status ? 1 : 0;
+    total += currentGrid[topRow][rightColumn].status ? 1 : 0;
+    total += currentGrid[ii][leftColumn].status ? 1 : 0;
+    total += currentGrid[ii][rightColumn].status ? 1 : 0;
+    total += currentGrid[bottomRow][leftColumn].status ? 1 : 0;
+    total += currentGrid[bottomRow][jj].status ? 1 : 0;
+    total += currentGrid[bottomRow][rightColumn].status ? 1 : 0;
 
-    for (let cells = 0; cells < cellCount; cells++) {
-      cellArray.push(<div id={cells + 1} className="cell"></div>);
-    }
-    return cellArray;
+    return total;
   };
+
+  const updateGrid = () => {
+    let previousGridState = cellGrid.slice();
+    let currentNeighborTotal = 0;
+    let newGrid = [];
+
+    for (let rowIndex = 0; rowIndex < gridHeight; rowIndex++) {
+      let newRow = [];
+      for (let colIndex = 0; colIndex < gridWidth; colIndex++) {
+        currentNeighborTotal = calculateNeighbors(
+          previousGridState,
+          rowIndex,
+          colIndex
+        );
+        let currentCell = previousGridState[rowIndex][colIndex];
+
+        if (!currentCell.status) {
+          if (currentNeighborTotal == 3) {
+            newRow.push({ status: true, newBorn: true });
+          } else {
+            newRow.push({ status: false });
+          }
+        } else if (currentNeighborTotal < 2) {
+          newRow.push({ status: false });
+        } else if (currentNeighborTotal > 3) {
+          newRow.push({ status: false });
+        } else {
+          newRow.push({ status: true });
+        }
+      }
+      newGrid.push(newRow);
+    }
+    setGrid(newGrid);
+    setGenerationCount(generationCount + 1);
+  };
+
+  const useInterval = (callback, delay) => {
+    const savedCallback = useRef();
+
+    useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  };
+
+  useInterval(
+    () => {
+      updateGrid();
+    },
+    isRunning ? delay : null
+  );
 
   return (
     <>
       <div className="gol-container">
         <h3 className="title">Conway's Game Of Life</h3>
         <div className="board">
-          {/* {createCells().map((index) => {
-            return (
-              <Cell
-                key={index}
-                flipState={() => {
-                  flipState(cell_index, row_index);
-                }}
-                alive={tableCell.status}
-                newBorn={tableCell.newBorn}></Cell>
-            );
-          })} */}
+          {cellGrid.map((tableRow, row_index) => {
+            return tableRow.map((tableCell, cell_index) => {
+              return (
+                <Cell
+                  key={cell_index}
+                  alive={tableCell.status}
+                  newBorn={tableCell.newBorn}></Cell>
+              );
+            });
+          })}
         </div>
         <Menu
         // randomizeGrid={randomizeGrid}
